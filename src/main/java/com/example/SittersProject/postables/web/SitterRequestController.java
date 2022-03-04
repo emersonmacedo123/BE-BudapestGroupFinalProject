7aa9038173d4fbbd7da00960b8607008e6383b56
Holden Hartsoe <holden.hartsoe@gmail.com>
2022-03-04 18:25:35 +0100

    still working on sitter request crud - having trouble with delete mapping

--- src/main/java/com/example/SittersProject/postables/web/SitterRequestController.java
+++ src/main/java/com/example/SittersProject/postables/web/SitterRequestController.java
@@ -6,10 +6,7 @@ import com.example.SittersProject.postables.services.SitterRequestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,11 +19,18 @@ public class SitterRequestController {
         this.sitterRequestService = sitterRequestService;
     }
 
-    @GetMapping("/all_requests")
+    @GetMapping("/sitter_search")
     public String viewAllRequests(Model model){
         List<SitterRequest> sitterRequestList = sitterRequestService.getAll();
         model.addAttribute("sitterRequests", sitterRequestList);
         System.out.println("viewAllRequests method triggered");
+        return "sitter_search";
+    }
+
+    @RequestMapping(value="/sitter_request/delete/{id}", method = RequestMethod.DELETE)
+    public String deleteSitterRequest(@PathVariable String id){
+        Long requestId = Long.parseLong(id);
+        sitterRequestService.removeSitterRequestDB(requestId);
         return "sitter_search";
     }
 
