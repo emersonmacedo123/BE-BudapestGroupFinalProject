e4cd49f0f175c52bf7a12acd10747c695917db69
Stevan65327 <82317671+Stevan65327@users.noreply.github.com>
2022-03-14 23:43:03 +0100

    enforced the login page

--- src/main/java/com/example/SittersProject/WebSecurityConfiguration.java
+++ src/main/java/com/example/SittersProject/WebSecurityConfiguration.java
@@ -39,6 +39,10 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated()
                 .and()
+                .formLogin()
+                .loginPage("/registration")
+                .permitAll()
+                .and()
                 .httpBasic();
     }
 }
