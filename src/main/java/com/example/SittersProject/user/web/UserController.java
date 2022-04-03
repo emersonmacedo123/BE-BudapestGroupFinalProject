ba58cf7a56e3c92158240b00afa38aa39e6ed275
Holden Hartsoe <holden.hartsoe@gmail.com>
2022-04-03 15:36:20 +0200

    testing for merge

--- src/main/java/com/example/SittersProject/user/web/UserController.java
+++ src/main/java/com/example/SittersProject/user/web/UserController.java
@@ -19,6 +19,7 @@ import org.springframework.web.servlet.view.RedirectView;
 import java.util.List;
 import java.util.Optional;
 
+//testing merge
 @Controller
 @CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
