bbf8a9a956f1f44ae082537f0e3da09b5b10bccc
Holden Hartsoe <holden.hartsoe@gmail.com>
2022-04-30 13:01:56 +0200

    implemented submitSitterRequest function and its cohorts

--- src/main/java/com/example/SittersProject/user/repository/UserRepository.java
+++ src/main/java/com/example/SittersProject/user/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.example.SittersProject.user.repository;
 
 import com.example.SittersProject.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
